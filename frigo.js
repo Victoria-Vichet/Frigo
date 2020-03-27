617b62b9e959f588071a519e56b77833b7c8ebc3
VV <victoria.vichet@gmail.com>
2020-03-27 18:01:17 +0100

    Affiche le frigo Ajoute un produit (supérieur à 1, inférieur à 15, pas de nombre à virgule)

---
+++ frigo.js
@@ -0,0 +1,49 @@
+const url ="http://webmmi.iut-tlse3.fr/~jean-marie.pecatte/frigos/26/produits";
+
+/* Lister le contenu du frigo */
+let contenuFrigo = document.getElementById("contenu");
+contenuFrigo.addEventListener("click", listerFrigo);
+
+function listerFrigo(event){
+  let fetchOptions = {method: 'GET'};
+  fetch(url, fetchOptions)
+    .then( (response) => {
+      return response.json()
+    })
+    .then( (dataJSON) => {
+      let listeFinale = document.getElementById("contenuFrigo");
+      let res ="";
+      for (let p of dataJSON) {
+        res = res + "<li>" + p.nom + "</li>";
+      }
+      listeFinale.innerHTML = res;
+    })
+    .catch( (error) => {
+      console.log(error)
+    })
+}
+
+/* Ajouter un nouveau produit au frigo */
+let ajoutProduit = document.getElementById("ajouter");
+ajoutProduit.addEventListener("click", ajouterProduit);
+
+function ajouterProduit(event){
+  let produitNom = document.getElementById("produitAjout").value;
+  let produitQte = document.getElementById("quantiteAjout").value;
+  let produit = {"nom": produitNom, "qte":produitQte};
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  const fetchOptions = {
+   method: 'POST',
+   headers: myHeaders,
+   body: JSON.stringify(produit)
+  }
+  fetch(url, fetchOptions)
+    .then( (response) => {
+      return response.json()
+    })
+    .then( (dataJSON) => {
+
+    })
+    .catch( (error) => console.log(error))
+}
