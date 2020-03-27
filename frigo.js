86f8a416e5415d9acc649d6bd4bd256ed7b4b097
VV <victoria.vichet@gmail.com>
2020-03-27 19:08:40 +0100

    Révision nommage HTML Recherche JS ok

--- frigo.js
+++ frigo.js
@@ -40,10 +40,38 @@ function ajouterProduit(event){
   }
   fetch(url, fetchOptions)
     .then( (response) => {
-      return response.json()
+      if (true) {
+        return response.json()
+      }else{
+        throw new Error('Le produit existe déjà');
+      }
     })
     .then( (dataJSON) => {
 
     })
     .catch( (error) => console.log(error))
+}
+
+/*Rechercher un produit dans le frigo */
+let chercheProduit = document.getElementById("rechercher");
+chercheProduit.addEventListener("click", chercherProduit);
+
+function chercherProduit(event){
+  let urlRecherche = url + '?search=' + document.getElementById("produitRecherche").value;
+  let fetchOptions = {method: 'GET'};
+  fetch(urlRecherche, fetchOptions)
+    .then( (response) => {
+      return response.json()
+    })
+    .then( (dataJSON) => {
+      let listeFinale = document.getElementById("produitExistant");
+      let res ="";
+      for (let p of dataJSON) {
+        res = res + "<li>" + p.nom + "</li>";
+      }
+      listeFinale.innerHTML = res;
+    })
+    .catch( (error) => {
+      console.log(error)
+    })
 }
